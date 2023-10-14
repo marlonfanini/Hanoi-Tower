4b907b9c80a1d680caf1e794894e724f8188f6af
Marlon Fanini <FaniniDev@outlook.com>
2023-10-13 22:21:58 -0400

    first commit

---
+++ hanoi.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void getDisks();
+void play(int n, char torre1, char torre2, char torre3);
+int readInt();
+int totalMoves = 0;
+
+void getDisks() {
+    int totalDiscos;
+    cout << "Ingrese el número de discos: ";
+    totalDiscos = readInt();
+    cout << endl;
+    cout << "Resolviendo Torres de Hanoi con " << totalDiscos << " discos" << endl;
+    play(totalDiscos , 'A', 'B', 'C');
+}
+
+void play(int n, char torre1, char torre2, char torre3) {
+    if (n == 1) {
+        cout << "Se mueve anillo 1 de torre " << torre1 << " a torre " << torre3 << endl;
+        totalMoves++;
+        return;
+    }
+    play(n - 1, torre1, torre3, torre2);
+    cout << "Se mueve anillo " << n << " de torre " << torre1 << " a torre " << torre3 << endl;
+    totalMoves++;
+    play(n - 1, torre2, torre1, torre3);
+}
+
+int readInt() {
+    int value;
+    while (!(cin >> value)) {
+        cout << "Ingrese un valor válido: ";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+    return value;
+}
+
+int main(){
+    char torre1 = 'A', torre2 = 'B', torre3 = 'C', n = 0;
+    bool menu = true;
+
+    while (menu) {
+        cout << "----- Torres de Hanoi ----- \n" << "Seleccione una opción: " << endl;
+        cout << "1. Resolver Torres de Hanoi" << endl;
+        cout << "2. Salir" << endl;
+        cout << "Opción: ";
+        int option = readInt();
+        cout << endl;
+
+        switch (option){
+            case 1:
+                getDisks();
+                cout << "Total de movimientos: " << totalMoves << endl;
+                break;
+            case 2:
+                cout << "Saliendo del programa...\n";
+                menu = false;
+                break;
+        }
+        cout << endl;
+        totalMoves = 0;
+    }
+}
